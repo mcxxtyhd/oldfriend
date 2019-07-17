3a01cae8665b95f03e2de117edb190f57d714f30
mcxxtyhd <353950534@qq.com>
2019-07-17 22:52:24 +0800

    初始化上传啊

---
+++ src/main/java/com/feng/oldfriend/OldfriendApplication.java
@@ -0,0 +1,13 @@
+package com.feng.oldfriend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OldfriendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OldfriendApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/feng/oldfriend/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.feng.oldfriend.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @RequestMapping("/theo")
+    public String test(){
+        return "123asdasd";
+    }
+}
